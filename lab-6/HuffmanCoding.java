62f7887faedab9585a7c2cb15a3167f6f5d6729b
devashish94 <roy.devashish94@gmail.com>
2023-09-29 14:36:31 +0530

    add Huffman Coding

--- lab-6/HuffmanCoding.java
+++ lab-6/HuffmanCoding.java
@@ -1,9 +1,11 @@
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 class HuffmanCoding {
-    private record Letter(char character, int freq) {}
+    private record Letter(char character, int freq) {
+        public String toString() {
+            return "(" + this.character + ", " + this.freq + ")";
+        }
+    }
 
     private record Node(String symbol, int value, Node left, Node right) {
         @Override
@@ -12,32 +14,39 @@ class HuffmanCoding {
         }
     }
 
-    private static void bfs(Node root) {
-        if (root == null) {
-            return;
-        }
-        List<Node> list = new LinkedList<>();
-        list.add(root);
+    static LinkedHashMap<Character, Integer> sorted(LinkedHashMap<Character, Integer> m) {
+        LinkedHashMap<Character, Integer> newMap = new LinkedHashMap<>();
+        List<Integer> list = new LinkedList<>();
 
-        while (!list.isEmpty()) {
-            Node node = list.remove(0);
-            System.out.print(node + " ");
-            if (node.left != null) {
-                list.add(node.left);
-            }
-            if (node.right != null) {
-                list.add(node.right);
+        for (Map.Entry<Character, Integer> e : m.entrySet()) {
+            list.add(e.getValue());
+        }
+        Collections.sort(list);
+
+        for (Integer i : list) {
+            for (Map.Entry<Character, Integer> e : m.entrySet()) {
+                if (e.getValue().equals(i)) {
+                    newMap.put(e.getKey(), i);
+                }
             }
         }
+        return newMap;
     }
 
     public static void main(String[] args) {
-        Letter[] input = {
-            new Letter('a', 5),
-            new Letter('b', 1),
-            new Letter('c', 6),
-            new Letter('d', 3),
-        };
+        String s = "aabbccccd";
+        LinkedHashMap<Character, Integer> m = new LinkedHashMap<>();
+        for (char c : s.toCharArray()) {
+            m.merge(c, 1, Integer::sum);
+        }
+        m = sorted(m);
+        List<Letter> input = new LinkedList<>();
+
+        for (Map.Entry<Character, Integer> entry : m.entrySet()) {
+            input.add(new Letter(entry.getKey(), entry.getValue()));
+        }
+
+        System.out.println(input);
 
         List<Node> nodes = new LinkedList<>();
         for (Letter letter : input) {
@@ -46,7 +55,6 @@ class HuffmanCoding {
 
         while (!nodes.isEmpty()) {
             nodes.sort(Comparator.comparingInt(node -> node.value));
-            System.out.println(nodes);
             if (nodes.size() == 1) {
                 break;
             }
@@ -57,6 +65,5 @@ class HuffmanCoding {
         }
 
         Node root = nodes.remove(0);
-        bfs(root);
     }
 }
